b10969cbafcd93fffd7f0a45e2c4f319fc930cde
ziad-mohamed-dev <ziadonwork@gmail.com>
2025-06-05 18:04:04 +0300

    import createTaskSchema

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,24 +1,22 @@
-
-const express = require('express');
-const router  = express.Router();                    
+const express = require("express");
+const router = express.Router();
 
 const { protect } = require("../middleware/authMiddleware");
 
-const { validateBody } = require('../middleware/validate');
-const { taskSchema }   = require('../validation/schemas');
+const { validateBody } = require("../middleware/validate");
+const { taskSchema, createTaskSchema } = require("../validation/schemas");
 
 const {
   getTasks,
   updateTask,
-  deleteTask
-} = require('../controllers/taskController');
+  deleteTask,
+} = require("../controllers/taskController");
 
-const { createTask } = require('../controllers/taskController');
+const { createTask } = require("../controllers/taskController");
 
-
-router.get('/',    protect, getTasks);
-router.post('/', protect, validateBody(createTaskSchema), createTask);
-router.put('/:id', protect, validateBody(taskSchema), updateTask);
-router.delete('/:id', protect, deleteTask);
+router.get("/", protect, getTasks);
+router.post("/", protect, validateBody(createTaskSchema), createTask);
+router.put("/:id", protect, validateBody(taskSchema), updateTask);
+router.delete("/:id", protect, deleteTask);
 
 module.exports = router;
