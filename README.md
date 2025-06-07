2adc919b573ff8504db1dc954316210ec359df78
ziad-mohamed-dev <ziadonwork@gmail.com>
2025-06-07 19:20:08 +0300

    throw an error if the NEXT_PUBLIC_BACKEND_BASE_URL  is not defined

--- README.md
+++
@@ -1,1 +0,0 @@
-# fullstack-task-board
--- frontend/src/utils/axiosInstant.ts
+++ frontend/src/utils/axiosInstant.ts
@@ -1,6 +1,12 @@
 import axios from "axios";
 
+const BACKEND_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL
+
+if (!process.env.NEXT_PUBLIC_BACKEND_BASE_URL) {
+  throw new Error('Environment variable NEXT_PUBLIC_BACKEND_BASE_URL is not defined');
+}
+
 export const AxiosInstant = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/api`,
+  baseURL: `${BACKEND_BASE_URL}/api`,
   withCredentials: true,
 });
