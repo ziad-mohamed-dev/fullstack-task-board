c8b30f0beefa577deccf28f37fbee5d9726d54ce
ziad-mohamed-dev <ziadonwork@gmail.com>
2025-06-09 09:11:14 +0300

    fix deployment url in frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: false,
+  env: {
+    NEXT_PUBLIC_BACKEND_BASE_URL: process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : 'http://localhost:5000',
+  },
 };
 
 export default nextConfig;
--- frontend/src/utils/axiosInstant.ts
+++ frontend/src/utils/axiosInstant.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const BACKEND_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:5000";
+const BACKEND_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
 
 if (!BACKEND_BASE_URL) {
   throw new Error('Environment variable NEXT_PUBLIC_BACKEND_BASE_URL is not defined');
