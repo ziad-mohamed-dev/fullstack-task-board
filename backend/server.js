ef8c94617a716c08f1af86a7808a0cc3d76247ad
ziad-mohamed-dev <ziadonwork@gmail.com>
2025-06-09 08:47:33 +0300

    config the vercel for vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: process.env.NODE_ENV === 'production' ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000',
   credentials: true
 }));
 
