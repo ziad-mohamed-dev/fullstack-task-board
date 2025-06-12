e75b837943c16180a65c510241cc3ce238c25898
ziad-mohamed-dev <ziadonwork@gmail.com>
2025-06-12 09:04:36 +0300

    roll back the app to normal

--- frontend/src/app/(app)/boards/[id]/page.tsx
+++ frontend/src/app/(app)/boards/[id]/page.tsx
@@ -1,54 +1,11 @@
 import BoardClient from "@/components/board/BoardClient";
+import { getBoard } from "@/utils/serverApi";
 
 const Board = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
-  // const board = (await getBoard(id as string)).data;
+  const board = (await getBoard(id as string)).data;
 
-  return (
-    <BoardClient
-      initBoard={{
-        _id: id,
-        name: "Getting Started",
-        description: "Your first board",
-        tasks: [
-          {
-            _id: "1",
-            icon: "⏰",
-            board: id,
-            name: "Task in Progress",
-            status: "In Progress",
-          },
-          {
-            _id: "2",
-            icon: "🏋️‍♀️",
-            board: id,
-            name: "Task Completed",
-            status: "Completed",
-          },
-          {
-            _id: "3",
-            icon: "☕",
-            board: id,
-            name: "Task Won’t Do",
-            status: "Won't Do",
-          },
-          {
-            _id: "4",
-            icon: "📚",
-            board: id,
-            name: "Task To Do",
-            description:
-              "Work on a Challenge on devChallenges.io, learn TypeScript.",
-            status: "To Do",
-          },
-        ],
-      }}
-    />
-  );
+  return <BoardClient initBoard={board} />;
 };
 
 export default Board;
-
-
-
-// Task To Do
