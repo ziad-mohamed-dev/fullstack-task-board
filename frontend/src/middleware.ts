b5d1b9c555d5ac53ce164a676fbf4eb1b9d889b8
ziad-mohamed-dev <ziadonwork@gmail.com>
2025-06-12 08:58:47 +0300

    just for apply

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,45 +1,45 @@
-import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { verifyToken } from "./utils/serverApi";
+// import { cookies } from "next/headers";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+// import { verifyToken } from "./utils/serverApi";
 
-const guestOnlyRoutes = ["/sign-in", "/sign-up"];
-const protuctedRoutes = ["/boards"];
+// const guestOnlyRoutes = ["/sign-in", "/sign-up"];
+// const protuctedRoutes = ["/boards"];
 
-const isGuestOnlyRoute = (pathname: string) => {
-  return guestOnlyRoutes.some((guestRoute) => pathname.startsWith(guestRoute));
-};
+// const isGuestOnlyRoute = (pathname: string) => {
+//   return guestOnlyRoutes.some((guestRoute) => pathname.startsWith(guestRoute));
+// };
 
-const isProtuctedRoute = (pathname: string) => {
-  return protuctedRoutes.some((protuctedRoute) =>
-    pathname.startsWith(protuctedRoute)
-  );
-};
+// const isProtuctedRoute = (pathname: string) => {
+//   return protuctedRoutes.some((protuctedRoute) =>
+//     pathname.startsWith(protuctedRoute)
+//   );
+// };
 
-export const middleware = async (request: NextRequest) => {
-  const { pathname } = request.nextUrl;
-  const cookieStore = await cookies();
-  const token = cookieStore.get("token")?.value;
+// export const middleware = async (request: NextRequest) => {
+//   const { pathname } = request.nextUrl;
+//   const cookieStore = await cookies();
+//   const token = cookieStore.get("token")?.value;
 
-  if (isGuestOnlyRoute(pathname) && token) {
-    try {
-      await verifyToken();
-      return NextResponse.redirect(new URL("/boards", request.url));
-    } catch {
-      return;
-    }
-  }
+//   if (isGuestOnlyRoute(pathname) && token) {
+//     try {
+//       await verifyToken();
+//       return NextResponse.redirect(new URL("/boards", request.url));
+//     } catch {
+//       return;
+//     }
+//   }
 
-  if (isProtuctedRoute(pathname)) {
-    if (token) {
-      try {
-        await verifyToken();
-        return NextResponse.next();
-      } catch {
-        return NextResponse.redirect(new URL("/sign-in", request.url));
-      }
-    } else {
-      return NextResponse.redirect(new URL("/sign-in", request.url));
-    }
-  }
-};
+//   if (isProtuctedRoute(pathname)) {
+//     if (token) {
+//       try {
+//         await verifyToken();
+//         return NextResponse.next();
+//       } catch {
+//         return NextResponse.redirect(new URL("/sign-in", request.url));
+//       }
+//     } else {
+//       return NextResponse.redirect(new URL("/sign-in", request.url));
+//     }
+//   }
+// };
